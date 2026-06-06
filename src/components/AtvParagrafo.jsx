4ecd8a6c72f77c3756bbb9aefed4a1ddee1abb8b
Vinicyus-Nave2D <vinicyus.eduardo@nave.org.br>
2026-06-06 15:17:51 -0300

    vz

--- src/components/AtvParagrafo.jsx
+++ src/components/AtvParagrafo.jsx
@@ -3,3 +3,13 @@
 
 Crie e exporte por padrão um componente chamado "AtvParagrafo" que recebe dois argumentos: um "titulo" e um "conteudo". O componente deve exibir uma <div> com um elemento <h1> e um elemento <p> internos. O <h1> terá o conteúdo do argumento "título" e <p> terá o conteúdo do argumento "conteudo". O <h1> de "titulo" deve ter o tamanho de 32px, e o <p> de "conteudo" deve ter o tamanho de 16px. O texto do "titulo" deve ficar acima do texto do "conteudo".
 */
+import root from '../styles/main';
+
+const estilo = {
+ titulo:
+
+ { fontSize: "32px" },
+ 
+ conteudo:
+
+ { fontSize: "16px" }, }
