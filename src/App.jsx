d9584ee29f1b17c01a2faa83f33e02e179379c17
brendha-priscilla-nave <brendha.priscilla@nave.org.br>
2026-06-08 17:20:14 +0000

    Atividade Concluida

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Divisoria from './components/Divisoria';
 import Paragrafo from './components/Paragrafo';
 import Botao from './components/Botao';
+import AtvDivisoria from './components/AtvDivisoria';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <section id="center">
+        <AtvDivisoria largura={20}/>
         <Divisoria>
           <Paragrafo conteudo="Sou um exemplo"/>
           <Botao/>
