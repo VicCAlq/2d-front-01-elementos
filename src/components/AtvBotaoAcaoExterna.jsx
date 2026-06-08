6e95cd73d0ab58c8bccb2b0df950e1c3ef40c014
caua-rafael-nave <caua.rafael@nave.org.br>
2026-06-08 14:02:05 -0300

    blackoutzz

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -3,15 +3,9 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
-import root from '../styles/main';
-
-export default function AtvBotaoAcaoExterna ({comando}){
-    return(
-        <div>
-            <p>Clique abaixo</p>
-            <button
-            onClick={() => {comando}}
-            >Clique aqui</button>
-        </div>
-    )
+export default function AtvBotaoAcaoExterna({comando})  {
+  return(<div>
+      <p>Clique abaixo</p>
+    <button onClick={comando}>Clique aqui</button>
+  </div>)
 }
--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -3,15 +3,9 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
-import root from '../styles/main';
-
-export default function AtvBotaoAcaoInterna (){
-    return(
-        <div>
-            <p>Clique abaixo</p>
-            <button
-            onClick = { () => console.log("Fui clicado!")} 
-            >Clique aqui</button>
-        </div>
-    )
+export default function AtvBotaoAcaoInterna()  {
+  return(<div>
+      <p>Clique abaixo</p>
+    <button onClick={() => alert("Fui clicado!")}>Clique aqui</button>
+  </div>)
 }
