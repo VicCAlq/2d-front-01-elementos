2ed71bae27622d5c9e21209a6a849fddf9afe302
emilly-maciel-nave26 <emilly.maciel@nave.org.br>
2026-06-08 12:39:23 -0300

    emilly

--- src/components/AtvParagrafo.jsx
+++ src/components/AtvParagrafo.jsx
@@ -3,3 +3,24 @@
 
 Crie e exporte por padrão um componente chamado "AtvParagrafo" que recebe dois argumentos: um "titulo" e um "conteudo". O componente deve exibir uma <div> com um elemento <h1> e um elemento <p> internos. O <h1> terá o conteúdo do argumento "título" e <p> terá o conteúdo do argumento "conteudo". O <h1> de "titulo" deve ter o tamanho de 32px, e o <p> de "conteudo" deve ter o tamanho de 16px. O texto do "titulo" deve ficar acima do texto do "conteudo".
 */
+import root from '../styles/main';
+
+export default function AtvDivisoria ({largura}){
+    const style = {
+    inicial: {
+    width: "90vw"
+    },
+    esquerda: {
+    width: toString(largura) + '%'
+    },
+    direita: {
+    width: toString(100-largura) + '%'
+    },
+}
+    return(
+        <div style={style.inicial}>
+        <div style={style.esquerda}> </div>
+        <div style={style.direita}> </div>
+        </div>
+    )
+}
