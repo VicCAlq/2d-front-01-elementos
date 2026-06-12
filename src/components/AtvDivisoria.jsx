a8133a34d17df3926cdbb30c3852073fc464d01b
imael-isaque-nave <imael.isaque@nave.org.br>
2026-06-12 15:25:38 -0300

    ta paz

--- src/components/AtvDivisoria.jsx
+++ src/components/AtvDivisoria.jsx
@@ -1,16 +1,13 @@
 /*
-[ATIVIDADE DIVISÓRIA]
+[ATIVIDADE BOTÃO COM AÇÃO INTERNA]
 
-Crie e exporte por padrão um componente chamado "AtvDivisoria" que recebe um argumento "largura", cujo valor pode ser entre 10 e 90. O componente deve exibir uma <div> que ocupe 90% da largura da tela visível (use a propriedade "width" com o valor "90vw" para isso), e dentro desta <div> devem ter duas outras <div> exibidas lado a lado. A <div> da esquerda deve ocupar uma percentagem da <div> inicial, essa percentagem é determinada pelo argumento "largura". A <div> da direita deve ocupar a largura restante da <div> inicial. Exemplo:
-
-argumento largura = 30
-<div> inicial = 90% da largura da tela
-<div> esquerda = 30% da largura da div inicial (30% de 90% da largura da tela)
-<div> direita = 70% da largura da div inicial (70% de 90% da largura da  tela)
+Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
-export default function AtvDivisoria({largura})  {
-  return(<div style={{ width: '90vw', display: 'flex' }}>
-    <div style={{ width: `${largura}%`, height: '20px', backgroundColor: '#4CAF50'}}></div>
-    <div style={{ width: `${100-largura}%`, height: '20px', backgroundColor: '#FF5722'}}></div>
-  </div>)
+export default function AtvBotaoAcaoInterna() {
+  return (
+    <div>
+      <p>Clique abaixo</p>
+      <button onClick={() => alert("Fui clicado!")}>Clique aqui</button>
+    </div>
+  );
 }
