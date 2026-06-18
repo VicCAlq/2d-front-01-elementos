98017aeb1078d868af8d5f24092e6959fa7e4d75
Wesley-Eder-Nave <wesley.eder@nave.org.br>
2026-06-18 09:54:48 -0300

    Update AtvParagrafo.jsx

--- src/components/AtvParagrafo.jsx
+++ src/components/AtvParagrafo.jsx
@@ -3,3 +3,24 @@
 
 Crie e exporte por padrão um componente chamado "AtvParagrafo" que recebe dois argumentos: um "titulo" e um "conteudo". O componente deve exibir uma <div> com um elemento <h1> e um elemento <p> internos. O <h1> terá o conteúdo do argumento "título" e <p> terá o conteúdo do argumento "conteudo". O <h1> de "titulo" deve ter o tamanho de 32px, e o <p> de "conteudo" deve ter o tamanho de 16px. O texto do "titulo" deve ficar acima do texto do "conteudo".
 */
+import root from '../styles/main';
+
+const estilo = {
+    titulo: {
+        fontSize: "32px"
+    },
+    conteudo: {
+        fontSize: "16px"
+    },
+}
+
+export default function AtvParagrafo ({titulo, conteudo}){
+return (
+    <div>
+        <h1 style={estilo.titulo}>{titulo}</h1>
+        <p style={estilo.conteudo}>{conteudo}</p>
+    </div>
+
+
+
+)}
