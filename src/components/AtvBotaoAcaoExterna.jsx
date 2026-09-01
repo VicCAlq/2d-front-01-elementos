087e55d1a0db9c2ebde6ad8b65f0cb77edca4809
samuel-assuncao-nave <samuel.assuncao@nave.org.br>
2026-09-01 10:48:37 -0300

    Implement AtvBotaoAcaoExterna component
    Create AtvBotaoAcaoExterna component that executes a command function on button click.

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -3,3 +3,18 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
+
+
+class "AtvBotaoAcaoExterna" function comando;
+<div>
+<p>
+  texto : "clique abaixo"
+</p>
+
+<button>
+tex:"clique aqui"
+
+</button>
+
+  if button == precionado esecute function comando;
+</div>
